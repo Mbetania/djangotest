2101b084fefe7bba7765a63e34f152be37f0d5d2
Mbetania <betaniamarianag@gmail.com>
2023-06-12 23:49:36 -0300

    jinja2 remove

--- myproject/settings.py
+++ myproject/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = [
+    'myapp/static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
