e0b836049e503b1b2ea288b2a96a8526dd33b477
Mbetania <betaniamarianag@gmail.com>
2023-06-13 23:25:52 -0300

    add url

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import add_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/', add_user, name=add_user)
 ]
