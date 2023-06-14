a2491972769daea58a4e5b7361ad9e7d727655ca
Mbetania <betaniamarianag@gmail.com>
2023-06-13 23:20:50 -0300

    views add

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import User
+from django.http import HttpResponseRedirect
 # Create your views here.
+def add_user(request):
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    user = User(name=name)
+    user.save()
+    return HttpResponseRedirect('/')
+
+  retur render(request, 'addUser.html')
