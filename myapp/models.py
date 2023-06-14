1cb9bad267b11d69f4d14aba368938e3f52bf53e
Mbetania <betaniamarianag@gmail.com>
2023-06-14 00:01:59 -0300

    fixed

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class User(model.Model):
-  name = models.CharFiel(max_length=100)
+class User(models.Model):
+  name = models.CharField(max_length=100)
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import User
+from django.http import HttpResponseRedirect
+
+def add_user(request):
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    user = User(name=name)
+    user.save()
+    return HttpResponseRedirect('/')
+
+  return render(request, 'addUser.html')
